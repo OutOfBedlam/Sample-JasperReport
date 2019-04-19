f3c4bd1eaf7af5cc7aeb24d94974f4c5205eb35e
Kwon, Yeong-Eon <eirny@uangel.com>
2019-04-19 18:01:56 +0900

    use file repository for image file

--- src/test/scala/sample/jasperreport/DatabaseReportTest.scala
+++ src/test/scala/sample/jasperreport/DatabaseReportTest.scala
@@ -1,6 +1,9 @@
 package sample.jasperreport
 
-import net.sf.jasperreports.engine.{JasperCompileManager, JasperExportManager, JasperFillManager}
+import java.util.Collections
+
+import net.sf.jasperreports.engine.{JasperCompileManager, JasperExportManager, JasperFillManager, SimpleJasperReportsContext}
+import net.sf.jasperreports.repo.{FileRepositoryPersistenceServiceFactory, FileRepositoryService, PersistenceServiceFactory, RepositoryService}
 import org.scalatest.{BeforeAndAfterAll, FlatSpec}
 import sample.db.Database
 
@@ -13,19 +16,35 @@ class DatabaseReportTest extends FlatSpec with BeforeAndAfterAll {
   }
 
   "jasper report" should "print with data" in {
+
+    // this repository service extension allows using relative path of resource in jrxml file
+    //
+    // https://community.jaspersoft.com/jasperreports-library/issues/10941
+    val ctx = new SimpleJasperReportsContext
+
+    val fileRepository = new FileRepositoryService(ctx, "./src/test/reports", false)
+    ctx.setExtensions(classOf[RepositoryService], Collections.singletonList(fileRepository))
+    ctx.setExtensions(classOf[PersistenceServiceFactory], Collections.singletonList(FileRepositoryPersistenceServiceFactory.getInstance))
+
     // 1. compile report from xml file
-    val jasperReport = JasperCompileManager.compileReport("./src/test/reports/demo_data.jrxml")
+
+    val jasperReport = JasperCompileManager.getInstance(ctx).compile("./src/test/reports/demo_data.jrxml")
+
     // 2. set parameters
     val params = new java.util.HashMap[String, AnyRef]()
     params.put("TITLE", "Database Example")
+
     // 3. get database connection
     val connection = db.ds.getConnection
+
     // 4. create JasperPrint
-    val jasperPrint = JasperFillManager.fillReport(jasperReport, params, connection)
+    val jasperPrint = JasperFillManager.getInstance(ctx).fill(jasperReport, params, connection)
+
     // 5. create pdf
-    JasperExportManager.exportReportToPdfFile(jasperPrint, "./src/test/reports/demo_data.pdf")
+    JasperExportManager.getInstance(ctx).exportToPdfFile(jasperPrint, "./src/test/reports/demo_data.pdf")
+
     // 6. create html
-    JasperExportManager.exportReportToHtmlFile(jasperPrint, "./src/test/reports/demo_data.html")
+    //JasperExportManager.getInstance(ctx).exportToHtmlFile(jasperPrint, "./src/test/reports/demo_data.pdf")
 
     // 7. view
     //net.sf.jasperreports.view.JasperViewer.viewReport(jasperPrint)
